18fc83d9f76ea2ba845edcaf662f4794bd5eff14
zhusiyi111 <625700571@qq.com>
2018-08-22 23:42:23 +0800

    logout

--- src/component/TopBar/Topbar.tsx
+++ src/component/TopBar/Topbar.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Icon } from '../../../node_modules/antd';
+import Cookies from 'js-cookie';
 
 interface TopbarProps {
 }
@@ -7,7 +9,7 @@ interface TopbarState {
 }
 
 const { BASE_DATA } = (window as any);
-const { pageId, dbSource } = BASE_DATA;
+const { pageId, dbSource, username } = BASE_DATA;
 
 class Topbar extends React.Component<TopbarProps, TopbarState> {
 
@@ -20,6 +22,11 @@ class Topbar extends React.Component<TopbarProps, TopbarState> {
         (document.querySelector('.J_previewContainer') as any).innerHTML = `
 		<iframe class="cd-canvas J_canvas" src="/page/preview.html?pageId=${pageId}&type=1&dbSource=${dbSource}">                
 		</iframe>`;
+    }
+
+    logout = () => {
+        Cookies.remove('pin');
+        location.href = '/api/page/login';
     }
 
     render() {
@@ -40,6 +47,10 @@ class Topbar extends React.Component<TopbarProps, TopbarState> {
                     <div className="d-line"></div>
                     <div className="J_publish d-publish">发布</div>
                     <div className="d-line"></div>
+                    <div className="d-username">
+                        <span>{username}</span>
+                        <Icon type="logout" onClick={this.logout} />
+                    </div>
                 </div>
             </div>
         )

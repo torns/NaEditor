bc4812f7ebf4d780d6dde8b99c26ade20227ccd8
zhusiyi111 <625700571@qq.com>
2018-09-24 22:33:03 +0800

    fix:server side // bug

--- config.js
+++ config.js
@@ -1,11 +1,21 @@
+const isServer = typeof window === 'undefined';
+
+let protocol;
+if (isServer) {
+    protocol = 'http://';
+} else {
+    protocol = '//';
+}
+
+
 let host;
-// host = `47.107.69.189`; //远程
-host = `127.0.0.1`; //本地
+host = `47.107.69.189`; //远程
+// host = `127.0.0.1`; //本地
 
 // dev
 module.exports = {
     host,
-    serverAddress: `//h5editor.cn`,
-    staticAddress: `//static.h5editor.cn`,
-    viewAddress: `//view.h5editor.cn`,
+    serverAddress: `${protocol}h5editor.cn`,
+    staticAddress: `${protocol}static.h5editor.cn`,
+    viewAddress: `${protocol}view.h5editor.cn`,
 }
